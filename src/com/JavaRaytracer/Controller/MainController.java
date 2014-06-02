d960296645da4f1f539aed40a6fd62734a988e1b
Michael Dillon <michael.dillon@gatech.edu>
2014-06-02 02:27:36 -0400

    Create GUI prototype for the raytracer.
    - Create FXML file for GUI layout.
    - Create application driver and controller classes.

---
+++ src/com/JavaRaytracer/Controller/MainController.java
@@ -0,0 +1,41 @@
+package com.JavaRaytracer.Controller;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.*;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+ 
+public class MainController implements Initializable {
+    
+    @FXML Button LdScnBtn;
+
+    public Stage appStage;
+
+    public MainController(Stage appStage) {
+        this.appStage = appStage;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+    
+    /**
+     * Method to open file dialog for selecting scenefile
+     */
+    public void loadScene(ActionEvent event) {
+        final FileChooser fileChooser = new FileChooser();
+        File sceneFile = fileChooser.showOpenDialog(appStage);
+        if (sceneFile != null) {
+            // Parse the file and store surface and lighting information
+        }
+    }
+}
